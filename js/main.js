88a4828a2c1f6008b89f39762dd04bd95f7fa776
RosyDiVilio <rosydivilio@gmail.com>
2024-05-06 12:47:14 +0200

    add: password generator

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const firstName = prompt ('Inserisci nome');
+const lastName = prompt ('Inserisci cognome');
+const color = prompt ('Inserisci il tuo colore preferito');
+const output = `
+----------Password Generator----------
+
+${firstName}${lastName}${color}21
+
+--------------------------------------
+`;
+console.log(output);
