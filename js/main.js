83a5bde5a949e4d153ecc41312d81a722ceb4b4e
RosyDiVilio <rosydivilio@gmail.com>
2024-05-07 18:02:50 +0200

    add: second and third bonus

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@ const lastName = prompt ('Inserisci cognome');
 const color = prompt ('Inserisci il tuo colore preferito');
 let numberOne = prompt ('scegli un numero da 10 a 20');
 let numberTwo = prompt ('scegli numero da 0 a 10');
-const output = `
-----------Password Generator----------
 
-${firstName}${lastName}${color}${parseInt(numberOne / numberTwo)}
+const output = `${firstName}${lastName}${color}${parseInt(numberOne / numberTwo)}`;
 
---------------------------------------
-`;
 console.log(output);
+
+document.getElementById ('pswgen').innerHTML = output; 
