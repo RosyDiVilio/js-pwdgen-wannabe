a342ce66865aea6fdeeb4ba6c67a4f4166ee5a40
RosyDiVilio <rosydivilio@gmail.com>
2024-05-07 13:06:14 +0200

    add: first bonus

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const firstName = prompt ('Inserisci nome');
 const lastName = prompt ('Inserisci cognome');
 const color = prompt ('Inserisci il tuo colore preferito');
+let numberOne = prompt ('scegli un numero da 10 a 20');
+let numberTwo = prompt ('scegli numero da 0 a 10');
 const output = `
 ----------Password Generator----------
 
-${firstName}${lastName}${color}21
+${firstName}${lastName}${color}${parseInt(numberOne / numberTwo)}
 
 --------------------------------------
 `;
